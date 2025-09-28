ff47e492c35ecad79a8213c4be017fc4652d1a26
Osada Paranaliyanage <osadalakmal@gmail.com>
2025-09-28 01:13:21 +0100

    cut a 0.1.3 release

--- Formula/parakeet-dictation.rb
+++ Formula/parakeet-dictation.rb
@@ -4,7 +4,7 @@ class ParakeetDictation < Formula
   desc "A dictation tool powered by Parakeet"
   homepage "https://github.com/osadalakmal/parakeet-dictation"
   url "https://github.com/osadalakmal/parakeet-dictation/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "c2c83c614edc749b5f8f56de146530e5d1d3c70295e2987ee1cbad8fdd93a7b3"
+  sha256 "51888410c1a833923987603d90aba9ae2525dc02acc8cc3883b95edcd3c6c26b"
   license "MIT"
   revision 2
 
